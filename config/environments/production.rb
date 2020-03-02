e0e51c0351610beec138a13f80092423cfe1ecfc
tylerraclin <traclin@uchicago.edu>
2020-03-02 19:43:47 +0000

    clear cache hopefully

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
-  config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -80,6 +78,9 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  # Code is not reloaded between requests
+  config.cache_classes = false
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
